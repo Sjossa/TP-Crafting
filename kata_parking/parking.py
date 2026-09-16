a201a2b25971c03d43cb990b6b5c8288750dcec1
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-09-16 10:21:36 +0200

    green: applique la remise de 60 pourcent pour les camions abonnes

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,7 +1,7 @@
 import math
 
 
-def calculer_prix(minutes):
+def calculer_prix(minutes, abonnee=False):
     if minutes <= 30:
         return 0
 
@@ -16,4 +16,9 @@ def calculer_prix(minutes):
 
     plafond_nombre_de_jour = calcul_nombre_de_jour * 18
 
-    return min(calcul_ajout_facturation, plafond_nombre_de_jour)
+    prix_final = min(calcul_ajout_facturation, plafond_nombre_de_jour)
+
+    if abonnee:
+        prix_final = prix_final * 0.60
+
+    return round(prix_final, 2)
