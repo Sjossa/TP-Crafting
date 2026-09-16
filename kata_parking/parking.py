b4c72f3df9c71faf67886ec3624fe6c282cba6ff
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-09-16 09:49:25 +0200

    green: implemente la facturation par demi-heure commencee

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,2 +1,15 @@
+import math
+
+
 def calculer_prix(minutes):
-    return 0
+    if minutes <= 30:
+        return 0
+
+    temps_depasse = minutes - 30
+
+    temp_suplementaire = temps_depasse / 30
+    calcul_tranche_de_30_minutes_depasserr = math.ceil(temp_suplementaire)
+
+    calcul_ajout_de_facturation = calcul_tranche_de_30_minutes_depasserr * 1.50
+
+    return calcul_ajout_de_facturation
