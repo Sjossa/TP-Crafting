3fbf4fb96f4e84a36d291180c041aa5fb2ee7182
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-09-16 09:41:18 +0200

    red: test de la facturation apres 30 minutes

--- kata_parking/test_parking.py
+++ kata_parking/test_parking.py
@@ -3,3 +3,8 @@ from parking import calculer_prix
 
 def test_un_stationnement_de_30_minutes_est_gratuit():
     assert calculer_prix(30) == 0
+
+
+def test_facturation_au_dela_de_30_minutes():
+
+    assert calculer_prix(31) == 1.50
