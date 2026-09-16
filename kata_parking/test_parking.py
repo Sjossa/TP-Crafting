c42424f1b299beee60d3586fb91f6fec5a51e645
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-09-16 09:58:02 +0200

    red: test plafond 18 euros par 24h

--- kata_parking/test_parking.py
+++ kata_parking/test_parking.py
@@ -8,3 +8,8 @@ def test_un_stationnement_de_30_minutes_est_gratuit():
 def test_facturation_au_dela_de_30_minutes():
 
     assert calculer_prix(31) == 1.50
+
+
+def test_facturation_pour_chaque_24h():
+    assert calculer_prix(480) == 18
+    assert calculer_prix(1440) == 18
