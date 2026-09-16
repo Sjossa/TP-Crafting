60cf23e13a8ad4b8d59dedb5702eec2496017575
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-09-16 10:06:58 +0200

    refactor: correction de l'orthographe des variables

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -7,13 +7,13 @@ def calculer_prix(minutes):
 
     temps_depasse = minutes - 30
 
-    temp_suplementaire = temps_depasse / 30
-    calcul_tranche_de_30_minutes_depasserr = math.ceil(temp_suplementaire)
+    temps_suplementaire = temps_depasse / 30
+    calcul_tranche_30_minutes_depasser = math.ceil(temps_suplementaire)
 
-    calcul_ajout_de_facturation = calcul_tranche_de_30_minutes_depasserr * 1.50
+    calcul_ajout_facturation = calcul_tranche_30_minutes_depasser * 1.50
 
     calcul_nombre_de_jour = math.ceil(minutes / 1440)
 
     plafond_nombre_de_jour = calcul_nombre_de_jour * 18
 
-    return min(calcul_ajout_de_facturation, plafond_nombre_de_jour)
+    return min(calcul_ajout_facturation, plafond_nombre_de_jour)
